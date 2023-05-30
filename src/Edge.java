c70de170341fb9f6009106a15f35db8a5ec9f472
zbekxz <forestnatplay@yandex.ru>
2023-05-30 09:34:59 +0600

    implementing constructor

--- src/Edge.java
+++ src/Edge.java
@@ -2,5 +2,9 @@ public class Edge<Vertex> {
     private Vertex source;
     private Vertex dest;
     private Double weight;
-
+    public Edge(Vertex source, Vertex dest, Double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
 }
