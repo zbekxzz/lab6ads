29369745980e45890021f470840569c6b782bd83
zbekxz <forestnatplay@yandex.ru>
2023-05-30 09:37:56 +0600

    implementing getters and setters

--- src/Edge.java
+++ src/Edge.java
@@ -7,4 +7,22 @@ public class Edge<Vertex> {
         this.dest = dest;
         this.weight = weight;
     }
+    public Vertex getSource() {
+        return source;
+    }
+    public Vertex getDest() {
+        return dest;
+    }
+    public Double getWeight() {
+        return weight;
+    }
+    public void setSource(Vertex source) {
+        this.source = source;
+    }
+    public void setDest(Vertex dest) {
+        this.dest = dest;
+    }
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
 }
